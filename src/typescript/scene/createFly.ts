b600147edfbcec85d4896c0fead2a3d35777a79a
jianghe <jianghe@uni-ubi.com>
2022-08-17 18:12:51 +0800

    优化飞线

--- src/typescript/scene/createFly.ts
+++ src/typescript/scene/createFly.ts
@@ -62,14 +62,6 @@ function setLine(points) {
 }
 // 放置飞线
 function setFlyLine(points) {
-    // for (let i = 0; i < points.length; i++) {
-    //     console.log('points', points[i]);
-    //     const geometry = new THREE.BoxGeometry(5, 5, 5);
-    //     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    //     const cube = new THREE.Mesh(geometry, material);
-    //     cube.position.copy(points[i])
-    //     lineGroup.add(cube);
-    // }
     var flyMesh = _Fly.addFly({
         color: getRandomColor(),
         curve: points,
